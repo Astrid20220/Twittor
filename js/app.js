84619a4c59dc4fab74a3c78777e787870684f26e
Astrid20220 <yadiragonzalez556@gmail.com>
2022-10-19 22:49:31 -0500

    Actualizar ruta del SW

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
+var url = window.location.href;
+var swLocation = '/twittor/sw.js';
 
-if(navigator.serviceWorked){
-    navigator.serviceWorker.register('/sw.js');
+
+if ( navigator.serviceWorker ) {
+
+
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register( swLocation );
 }
 
 
